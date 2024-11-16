afa1fb9ab4828c957f0f0a5fc6707b65d1fc4056
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-17 01:24:12 +0530

    this is fourt three commit

---
+++ shortestjsprogram.js
@@ -0,0 +1,5 @@
+/**do you know sortest javascript program , here it is the empty file,even though this file is empty there is nothing to do still js engine do lot of thing behind the since
+ * so lets open the source in the browser and set the debugger on empty file then a call stack created in which globaln execution context occur.
+ * so when we run the empty progrma then a global execution context is created, so javascript engine create something known as wimdow
+ * when you open console and write window then something output come as like a object, if you write this then you also get something like object
+ */
