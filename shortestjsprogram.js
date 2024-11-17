e45c0c2873a7753154a3435576198c90617be3c9
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-17 08:56:26 +0530

    this is my fourty three commit

--- shortestjsprogram.js
+++ shortestjsprogram.js
@@ -1,5 +1,10 @@
 /**do you know sortest javascript program , here it is the empty file,even though this file is empty there is nothing to do still js engine do lot of thing behind the since
  * so lets open the source in the browser and set the debugger on empty file then a call stack created in which globaln execution context occur.
  * so when we run the empty progrma then a global execution context is created, so javascript engine create something known as wimdow
+ * in window lot of method function come this is created by javasript engine and in global execution context, and just like window javascript engine create one more thing which is this
  * when you open console and write window then something output come as like a object, if you write this then you also get something like object
+ * and at the global level this this points to the window object, but
+ * what is window window is global object which is created along with global execution context
+ * 
+ * so whenever any javascript program is run a global object is created a global execution context is created and along with global execution cotext a this variable is created
  */
