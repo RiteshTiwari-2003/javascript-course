3e44f970b97e116ccb12803bcdf1e322956f83f8
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-18 23:32:17 +0530

    this is my sixty one commit

---
+++ callbackfunction.js
@@ -0,0 +1,14 @@
+/**what is the calllback function in the javascriipt?
+ * javascript is the synchronous and single threaded language
+ * blocking the main thread
+ * power of callback?
+ * deep about event listeners?
+ * closure demo with event listener?
+ * scope demo with event listener?
+ * garbage collection and removeeventlistner
+ * 
+ * 
+ * this functipon which you passed in another function is known as callback function , callback function is most powerful function inside the javascript it give access to the whole asynchronous world in synchronous threaded language.
+ * and javascript is do only one execution at a time in a synchronous order.but due to callback we can do async thing inside the javascript.
+ * 
+ */
