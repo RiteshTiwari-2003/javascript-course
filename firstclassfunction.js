34b82db0fc7685b3a2395eb6bf4545bf66750130
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-18 23:06:53 +0530

    this is my seventy commit

--- firstclassfunction.js
+++ firstclassfunction.js
@@ -32,5 +32,63 @@ b();
  * functiion (){
  * }but if you run this then error come thst uncaught syntex error: function statement is require a name,
  * annonymous function are used where function are used as a value,mens you need to assign in some variable to this function
+ * but when you access function a and function b after the declaration then  output come in right manner but if you write code like this
+ * function a(){
+ * console.log("a called");
+ * }
+ * var b=function xyz(parameter1,parameter2){
+ * console.log("b called");}
+ * a(argument1,argument2);
+ * b();
+ * xyz();// output come uncaught referenc3e error:xyz is not defined 
  * 
+ * 
+ * first class function
+ * 
+ * what is the first class function? instead of above these argument you can pass function as a argument or parameter in another function
+ * like annonymous function some time work as value and variable so you can pas an argument a annonymous function
+ * var b=function (paraqm1){
+ * console.log(param1);}
+ * b(function (){
+ * })
+ * 
+ * or you can pass a named function also in another function as argument 
+ * var b=function (param1){
+ * console.log(param1);}
+ * function x(){
+ * }
+ * b(x);
+ * 
+ * you can also return a function from a function
+ * you return a annonymous function from a function
+ * var b=function (param1){
+ * return function (){
+ * }
+ * }
+ * console.log(b());// in all this code output come a function whole body f(){}
+ * 
+ * then first class function is the ability to convert function as a value
+ * the ability of function to used as value is known as first class function , in javascript, first class functionn  are function that are treated as first class citizen,
+ * meaning they can be:
+ * assigned to variable
+ * passed as argument to the other function
+ * returned from other function
+ * characterstics of first class function:
+ * 1. assign yto a variable:
+ * const greet=function(name){
+ * return `Hello , ${name}!`;};
+ * console.log(greet('alice'))
+ * 2. passed as a arguments: funcction canbe passed s argument to the other function (higher order function)
+ * function executefunction(fn,value){
+ * return fn(value);}
+ * 
+ * const double=(x)=>x*2;
+ * console.log(executefunction(double,5));
+ * 3. return from a function :
+ * function createMultiplier(multiplier){
+ * return function (num){
+ * return num*multiplier;};
+ * }
+ * const triple=createMultiplier(3);
+ * console.log(triple(4));
  */
